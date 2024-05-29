571aa7ce4509d20f8ad6c56ba56e1bf47d4894ec
Oleg Tiaglov <o.v.tiaglov@foxford.ru>
2024-05-29 14:46:37 +0200

    weather

--- src/message_parsers/is_misrali.ts
+++ src/message_parsers/is_misrali.ts
@@ -1,6 +1,10 @@
 import TelegramBot = require("node-telegram-bot-api");
 
 export function isMisrali(input: TelegramBot.Message) {
+  if (!input.text) {
+    return false
+  }
+
   if (input.text.toLowerCase().includes('мисрали')) {
     return true
   }
