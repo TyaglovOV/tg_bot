b94745670ec7c1e3db157a1a5e2134503522aaec
Oleg Tiaglov <o.v.tiaglov@foxford.ru>
2024-05-29 14:36:24 +0200

    weather

--- src/message_parsers/weather.ts
+++ src/message_parsers/weather.ts
@@ -1,11 +1,11 @@
 import TelegramBot = require("node-telegram-bot-api");
 
 export function isAskForWeather(input: TelegramBot.Message) {
-  const splitted = input.text.split(' ')
-
-  if (splitted[0] === '!погода' || splitted[0] === '!gjujlf') {
-    return true
-  }
+  // const splitted = input.text.split(' ')
+  //
+  // if (splitted[0] === '!погода' || splitted[0] === '!gjujlf') {
+  //   return true
+  // }
 
   return false
 }
